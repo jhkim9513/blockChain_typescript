471bbc44fc28554adbae59b883170e83e515090e
jhkim9513 <dondon9513@gmail.com>
2021-11-08 12:10:11 +0900

    Im listening to a chapter #0.4

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("hello");
+const name = "kim", gender = "male", age = 29;
+
+const sayHi = (name, age, gender?) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+}
+
+sayHi(name, age, gender);
+
+export {}
